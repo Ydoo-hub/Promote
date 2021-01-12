6b2162272569437c072a4e8e2b64d0f50338d39a
ydoo <43613589@qq.com>
2021-01-12 16:13:15 +0800

    '查找，深度'

--- 数据结构/二叉查找树.js
+++ 数据结构/二叉查找树.js
@@ -16,7 +16,9 @@ function BST() {
     this.preOrder = preOrder;
     this.getMin = getMin;
     this.getMax = getMax;
-
+    this.find = find;
+    this.remove = remove;
+    this.maxDepth = maxDepth;
 }
 
 function insert(data) {
@@ -58,15 +60,15 @@ function inOrder(node) { // 中序遍历
 function preOrder(node) {  // 先序遍历
     if (node !== null) {
         console.log(node.show() + ' ');
-        inOrder(node.left);
-        inOrder(node.right);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 }
 
 function postOrder(node) {  // 后序遍历
     if (node !== null) {
-        inOrder(node.left);
-        inOrder(node.right);
+        postOrder(node.left);
+        postOrder(node.right);
         console.log(node.show() + ' ');
     }
 }
@@ -79,6 +81,7 @@ function getMin() {  // 拿到最小值，一直在左节点
     return current.data;
 }
 
+
 function getMax() {  // 拿到最大值，一直在右节点
     let current = this.root;
     while(current.right !== null){
@@ -86,6 +89,84 @@ function getMax() {  // 拿到最大值，一直在右节点
     }
     return current.data;
 }
+
+function find(data) { // 查找固定节点
+    let current = this.root;
+    while (current !== null) {
+        if (data > current.data) {
+            current = current.right;
+        } else if (data < current.data) {
+            current = current.left
+        } else {
+            return current;
+        }
+    }
+    return '不存在该节点';
+}
+
+function remove(data) { // 移出节点
+    let root = removeNode(this.root, data);
+    console.log('108',root);
+}
+
+function removeNode(node, data) {
+    
+    function getSmallest(node) {  // 拿到最小值，一直在左节点
+        let current = node;
+        console.log(current);
+        while(current.left !== null){
+            current = current.left;
+        }
+        return current;
+    }
+    if (node === null) {
+        return null;
+    }
+    if (data === node.data) {
+        // 没有子节点的节点
+        if (node.left === null && node.right === null) {
+            return null;
+        }
+        // 没有左子节点
+        if (node.left === null) {
+            return node.right;
+        }
+        // 没有右子节点
+        if (node.right === null) {
+            return node.left;
+        }
+        // 左右子节点都有
+        // console.log(node.right.data)
+        var tempNode = getSmallest(node.right);
+        // console.log(tempNode);
+
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        // console.log(node.right);
+        return node;
+    }
+    else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+    }
+    else {
+        node.right = removeNode(node.right, data);
+    }
+}
+
+function maxDepth(root) {  // 深度
+    if (root === null) { //注意等号
+        return 0;
+    } else {
+        let leftDepth = maxDepth(root.left),
+            rightDepth = maxDepth(root.right);
+
+        let childDepth = leftDepth > rightDepth ? leftDepth : rightDepth;
+
+        return childDepth + 1;//根节点不为空高度至少为1
+    }
+}
+
 
 var bst = new BST();
 bst.insert(23);
@@ -95,20 +176,30 @@ bst.insert(37);
 bst.insert(3);
 bst.insert(99);
 bst.insert(22);
-bst.insert(12);
+bst.insert(61);
 bst.insert(11);
-bst.insert(65);
-console.log('中序遍历');
-inOrder(bst.root);
-console.log('-------------')
-console.log('先序遍历');
-preOrder(bst.root);
-console.log('-------------')
-console.log('后序遍历');
-postOrder(bst.root);
-console.log('-------------')
+bst.insert(12);
+
+
 console.log('最小数值');
 console.log(bst.getMin());
-console.log('-------------')
+console.log('-------------');
 console.log('最大数值');
 console.log(bst.getMax());
+console.log('-------------');
+console.log('查找值');
+console.log(bst.find(65));
+console.log('-------------');
+bst.remove(23);
+
+console.log('-------------');
+console.log('中序遍历');
+inOrder(bst.root);
+console.log('-------------');
+console.log('先序遍历');
+preOrder(bst.root);
+console.log('-------------');
+console.log('后序遍历');
+postOrder(bst.root);         
+
+console.log(maxDepth(bst.root));
